8f188ca9a487529d59acce593e408d5b1fe4dbfc
Jessica <jkelly@urban.org>
2018-02-15 14:20:25 -0500

    :question: a little error checking

--- guess.py
+++ guess.py
@@ -16,17 +16,26 @@ youAreRight = False
 
 #pick a random number beween 1 and 10 and saves it into the variable num
 num = random.randint(1, 10)
+name = str(raw_input("What is your name? "))
 
-print "I'm thinking of an number between 1 and 10. Guess it!"
+print "Hi ", name, "! I'm thinking of an number between 1 and 10. Guess it!"
 
 #keep asking the user to guess until they get it right.
 while not youAreRight:
 	#this asks the user their guess and then saves it as an integer into the variable theGuess
-	theGuess = int(raw_input("Your guess: "))
+	theGuess = raw_input("Your guess: ")
+	try:
+		theGuess = int(theGuess)
+	except ValueError:
+		print "That's not a number! You lose."
 
 	#check if they are right
 	if theGuess == num:
 		print "Great Job!"
 		youAreRight = True   # set to True so that we stop looping/asking to guess.
 	else:
+		if theGuess > num:
+			print "Your guess is too high."
+		else:
+			print "Your gess is too low"
 		print "Try Again."
